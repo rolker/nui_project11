1f8c93ca7e5526a7498f5aa402adf8aa91b85169
Field User <field@ccom.unh.edu>
2022-05-13 11:26:38 +0000

    Add basic UDP nodes, wip

--- nodes/udp_to_sms.py
+++ nodes/udp_to_sms.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 
+import rospy
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind('', 5000)
+from std_msgs.msg import String
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('', 5000))
+s.settimeout(0.1)
+
+rospy.init_node("udp_to_sms")
+
+usbl_pub = rospy.Publisher("/project11/drix_8/usbl_modem/send_raw", String, queue_size=1)
+
+while not rospy.is_shutdown():
+  try:
+    data = s.recv(2048)
+    if len(data) and data[0] == '<':
+      usbl_msg = String()
+      usbl_msg.data = data
+      usbl_pub.publish(usbl_msg)
+  except socket.timeout:
+    pass
---
+++ nodes/usbl_to_udp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import rospy
+import socket
+
+from std_msgs.msg import String
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def onRaw(msg):
+  s.sendto(msg.data, ('192.168.8.235',5000))
+
+rospy.init_node("usbl_to_udp")
+
+rospy.Subscriber("/project11/drix_8/usbl_modem/raw", String, onRaw)
+
+rospy.spin()
